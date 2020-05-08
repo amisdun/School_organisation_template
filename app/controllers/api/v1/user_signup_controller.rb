444b897bfc13f1286591e16a8bb4b91befa26d74
amisdun <michealdunamis@gmail.com>
2020-05-08 08:01:26 +0000

    added a token verification for admin to signup users in a school

--- app/controllers/api/v1/user_signup_controller.rb
+++ app/controllers/api/v1/user_signup_controller.rb
@@ -1,24 +1,48 @@
 class Api::V1::UserSignupController < ApplicationController
   def create
-    user = User.where({:email => params[:email]}).first
 
-    if user
+    admin_token = request.headers['Authorization']
 
-      return render json: {res: "Email already exist"}, status: :unprocessable_entity
+    admin_token = admin_token.split(' ').last
 
-    else
-      password_hash = BCrypt::Password.create(params[:password])
+    if admin_token
 
-      new_user =  User.new({
-                  :email => params[:email],
-                  :password => password_hash,
-                  :category_name => params[:category_name]
-                })
+      decoded_token = JWT.decode admin_token, Rails.application.secrets.secret_key_base, true , {algorithm: 'HS256'}
 
-      if new_user.save
-        return render json: {res: "created"}, status: :created
+      admin = Admin.where({:_id => decoded_token[:user_id]).first
+
+      if admin 
+
+        user = User.where({:email => params[:email]}).first
+
+        if user
+
+          return render json: {res: "Email already exist"}, status: :unprocessable_entity
+    
+        else
+          password_hash = BCrypt::Password.create(params[:password])
+    
+          new_user =  User.new({
+                      :email => params[:email],
+                      :password => password_hash,
+                      :category_name => params[:category_name]
+                    })
+    
+          if new_user.save
+
+            return render json: {res: "created"}, status: :created
+          end
+    
+        end
+      else
+
+        return render json: {res: "Unauthorized"}, status: :unauthorized
       end
-
+    else
+      
+      return render json: {res: "provide user token in header"}, status: :unauthorized
     end
+
+    
   end
 end
